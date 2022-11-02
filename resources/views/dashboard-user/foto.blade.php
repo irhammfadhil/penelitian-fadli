28425a83059a4f90a97b3c01dc344a72deb2c15b
Irham Muhammad Fadhil <irham.mfadhil@gmail.com>
2022-11-03 00:28:07 +0700

    update

--- resources/views/dashboard-user/foto.blade.php
+++ resources/views/dashboard-user/foto.blade.php
@@ -94,20 +94,20 @@
                         <div class="md-step-bar-left"></div>
                         <div class="md-step-bar-right"></div>
                         </div>
-						<div class="md-step active col-lg-1 col-md-1" onclick="location.href='/informed-consent';" style="cursor: pointer;">
+						{{--<div class="md-step active col-lg-1 col-md-1" onclick="location.href='/informed-consent';" style="cursor: pointer;">
                         <div class="md-step-circle"><span>2</span></div>
                         <div class="md-step-title">Informed Consent</div>
                         <div class="md-step-bar-left"></div>
                         <div class="md-step-bar-right"></div>
-                        </div>
+                        </div>--}}
                         <div class="md-step active col-lg-1 col-md-1" onclick="location.href='/foto-gigi';" style="cursor: pointer;">
-                        <div class="md-step-circle"><span>3</span></div>
+                        <div class="md-step-circle"><span>2</span></div>
                         <div class="md-step-title">Foto Gigi</div>
                         <div class="md-step-bar-left"></div>
                         <div class="md-step-bar-right"></div>
 						</div>
 						<div class="md-step col-lg-1 col-md-1" onclick="location.href='/finalisasi';" style="cursor: pointer;">
-                        <div class="md-step-circle"><span>4</span></div>
+                        <div class="md-step-circle"><span>3</span></div>
                         <div class="md-step-title">Finalisasi Data</div>
                         <div class="md-step-bar-left"></div>
                         <div class="md-step-bar-right"></div>
